2688e4b3f44961a8bba689ea6fadd8e092df9e9b
r3lic <87726096+r3lic-pnw@users.noreply.github.com>
2024-11-01 06:14:18 +0000

    Added a clock and some more styles. Set up two column main area.

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,8 @@ const dropdown = document.querySelector(".dropdown");
 const menuIcon = document.querySelector("i");
 const dropdownContent = document.querySelector(".dropdown-content");
 
+updateDateTime();
+setInterval(updateDateTime, 1000);
 
 if (isTouchDevice){
   dropdown.ontouchstart = (e) => {
@@ -18,3 +20,10 @@ if (isTouchDevice){
   }
 
 } 
+
+function updateDateTime () {
+  const timeSpan = document.querySelector("#time-span");
+  const currentTime = new Date();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};
+  timeSpan.textContent = currentTime.toLocaleDateString('en-US', options);
+}
