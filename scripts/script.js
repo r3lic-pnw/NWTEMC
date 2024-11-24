ad49e85cc16690d0276aba3e3d87408b00e6a0ed
r3lic <87726096+r3lic-pnw@users.noreply.github.com>
2024-11-24 21:59:07 +0000

    Working on the contacts page.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-const isTouchDevice = "ontouchstart" in window;
 const dropdown = document.querySelector(".dropdown");
 const menuIcon = document.querySelector("i");
 const dropdownContent = document.querySelector(".dropdown-content");
@@ -6,24 +5,23 @@ const dropdownContent = document.querySelector(".dropdown-content");
 updateDateTime();
 setInterval(updateDateTime, 1000);
 
-if (isTouchDevice){
-  dropdown.ontouchstart = (e) => {
-    menuIcon.classList.toggle("fa-angle-down");
-    menuIcon.classList.toggle("fa-angle-right");
-    dropdownContent.classList.toggle("hidden");
-  }
-} else {
-  dropdown.onclick = (e) => {
-    menuIcon.classList.toggle("fa-angle-down");
-    menuIcon.classList.toggle("fa-angle-right");
-    dropdownContent.classList.toggle("hidden");
-  }
+dropdown.ontouchstart = (e) => {
+  menuIcon.classList.toggle("fa-angle-down");
+  menuIcon.classList.toggle("fa-angle-right");
+  dropdownContent.classList.toggle("hidden");
+}
 
-} 
+dropdown.onclick = (e) => {
+  menuIcon.classList.toggle("fa-angle-down");
+  menuIcon.classList.toggle("fa-angle-right");
+  dropdownContent.classList.toggle("hidden");
+}
 
-function updateDateTime () {
+
+
+function updateDateTime() {
   const timeSpan = document.querySelector("#time-span");
   const currentTime = new Date();
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
   timeSpan.textContent = currentTime.toLocaleDateString('en-US', options);
 }
